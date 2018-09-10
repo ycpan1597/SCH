730fb8a07dfc72baa96a327625526b91e8cb28b0
ycpan1597 <ycpan@uw.edu>
2018-09-10 07:02:41 -0700

    committed changes

--- accelClient.py
+++ accelClient.py
@@ -22,7 +22,21 @@ for a, b in zip(TD08.UTM, TD08s.UTM):
     TD08sum[1].append(np.std(a))
     TD08sum[2].append(np.average(b))
     TD08sum[3].append(np.std(b))
+    
+#%%     
 TD08sum = np.array(TD08sum)
+import matplotlib.mlab as mlab
+for i in range(6):
+    plt.figure()
+    plt.title(TD08.titles[i])
+    mu1, std1, mu2, std2 = TD08sum[:, i]
+    x1 = np.linspace(mu1 - 3 * std1, mu1 + 3 * std1, 100)
+    x2 = np.linspace(mu2 - 3 * std2, mu2 + 3 * std2, 100)
+    plt.plot(x1, mlab.normpdf(x1, mu1, std1), alpha = 0.5)
+    plt.plot(x2, mlab.normpdf(x2, mu2, std2), alpha = 0.5)
+    plt.xlabel('magnitude')
+    plt.ylabel('probability density')
+    plt.legend(['awake', 'asleep'])
 #l = initialize(['03', '04', '06', '08', '09'], ['01', '02', '03', '04', '05'], os, filetype) #for initializing multiple files
 #for item in l:
 #    item.ECDF(n = 50, kind = 'mag')
